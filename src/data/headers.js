065dc88ce96f350567858884e94c8a38bc7f071b
SapphicBear <sleepyscreenart@gmail.com>
2026-05-02 20:04:46 +0200

    add header and title for upload page

--- src/data/headers.js
+++ src/data/headers.js
@@ -2,6 +2,7 @@ const headers = {
     index: "Schublade",
     signIn: "Sign In",
     signUp: "Sign Up",
+    upload: "Upload files",
 }
 
 export default headers;
--- src/data/titles.js
+++ src/data/titles.js
@@ -2,6 +2,7 @@ const titles = {
     index: "Schublade",
     signIn: "Sign in to Schublade",
     signUp: "Sign up and share files",
+    upload: "Upload your files",
         
 };
 export default titles;
