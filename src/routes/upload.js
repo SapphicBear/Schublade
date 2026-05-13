5ae5c09cb6e7f54981817fdbf1fb242b0a471b07
SapphicBear <sleepyscreenart@gmail.com>
2026-05-13 15:53:18 +0200

    connect multer middleware to upload route

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import uploadCon from "./../controllers/upload-con.js";
+import upload from "./../data/multer.js";
 
 const uploadRouter = Router();
 
 uploadRouter.get("/", uploadCon.get);
+// uploadRouter.post("/", upload.single("test"));
 
 export default uploadRouter;
