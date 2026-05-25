fc3d208221cff9f2f941189985230d8bd70b808c
SapphicBear <sleepyscreenart@gmail.com>
2026-05-25 18:31:56 +0200

    make changes to prisma type and upload con

--- src/controllers/upload-con.js
+++ src/controllers/upload-con.js
@@ -1,6 +1,7 @@
 import links from "./../data/links.js";
 import titles from "./../data/titles.js";
 import headers from "./../data/headers.js";
+import prisma from "../../lib/prisma.js";
 
 const content = {
     title: titles.upload,
@@ -12,10 +13,21 @@ const content = {
 const uploadCon = {
     async get(req, res) {
         content.user = req.user;
+        content.folders = await prisma.folder.findMany({ where: { userId: req.user.id }});
         res.render("upload", content);
     },
     async post(req, res) {
         console.log(req.file);
+        console.log(req.body.type)
+        // upload file url to database
+        await prisma.file.create({
+            data: {
+                name: req.file.originalname,
+                userId: req.user.id,
+                type: req.file.mimetype,
+                url: req.file.path,
+            }
+        });
         res.redirect("/");
     }
 };
