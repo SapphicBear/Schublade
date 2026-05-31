f3b9df168b02337f366a3f63f1176d81dd3f4d5d
SapphicBear <sleepyscreenart@gmail.com>
2026-05-31 13:36:16 +0200

    Add feature for user made folders
    The user can now make folders and they appear on their home
    page.

--- src/controllers/new-folder.js
+++ src/controllers/new-folder.js
@@ -1,10 +1,17 @@
+import prisma from "../../lib/prisma.js";
+
 const folderController = {
-    post(req, res) {
+    async post(req, res) {
         console.log(req.body);
         if (!req.user) {
             return;
         }
-
+        await prisma.folder.create({
+            data: {
+                name: req.body["folder-name"],
+                userId: req.user.id,
+            }
+        });
         res.redirect("/");
     }
 }
