3203f75a6d0ab274f0f33b3c7e30751382225383
SapphicBear <sleepyscreenart@gmail.com>
2026-06-13 17:47:48 +0200

    fix typo

--- src/controllers/upload-con.js
+++ src/controllers/upload-con.js
@@ -37,11 +37,7 @@ const uploadCon = {
             }
             const { fileURL } = supabase.storage
                 .from("Files")
-<<<<<<< HEAD
-                .getPublicURL(file.originalname);
-=======
                 .getPublicUrl(file.originalname);
->>>>>>> 26939b4 (get public url for files)
             return fileURL.publicUrl;
         };
         const fileURL = await handleUpload(req.file);
